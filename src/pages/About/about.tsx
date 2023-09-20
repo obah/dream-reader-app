24a14bb4aeb1fb0d799ebe0175a55a2f4ad45b02
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-09-20 01:24:03 +0100

    feat: added more content to about page

--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,42 +1,62 @@
 import { Layout } from "../../components/layout";
+import AboutImage from "../../assets/images/about-flare.png";
 import "./styles/about.css";
 
 export function About() {
   return (
     <Layout>
       <section className="about__section">
-        <div className="about__top-text">
+        <div className="about__section-image">
+          <img src={AboutImage} alt="dream reader logo" />
+        </div>
+        <div className="about__section-top-text">
           <h1>What is Dream Reader?</h1>
-          <p>
-            It is a simple application to help readers create a list of the
-            books they are currently reading, plan to read and have read
-          </p>
+          <div>
+            <p>
+              It is an application that help readers create a list of the books
+              they are currently reading, have read or plan to read.
+            </p>
 
-          <p>
-            The application is built on the Google Book Library and relies
-            heavily on the Google Book API
-          </p>
+            <p>
+              The application is built on the Google Book Library and relies
+              heavily on the <span>Google Book API</span>
+            </p>
+
+            <p>
+              You can view the code here on{" "}
+              <a
+                href="https://github.com/obah/dream-reader-app"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github
+              </a>
+              . Any feedback or contribution is very much welcome
+            </p>
+          </div>
         </div>
 
-        <div className="about__final-text">
+        <div className="about__section-final-text">
           <h1>How can I use Dream Reader?</h1>
-          <p>
-            Just search for the book you want to access, then add it to your
-            Reading List
-          </p>
+          <div>
+            <p>
+              Just search for the book you want to access, then add it to your
+              Reading List.
+            </p>
 
-          <p>
-            From there, you can view it on Google Books and if a free digital
-            verison is availble, you can read it online or download it
-          </p>
+            <p>
+              From there, you can view it on Google Books and if a free digital
+              verison is availble, you can read it online or download it.
+            </p>
 
-          <p>You can also choose to remove a book from your Reading List</p>
+            <p>You can also choose to remove a book from your Reading List.</p>
 
-          <p>
-            Finally, you do not need an account to use Dream Reader, your
-            Reading List is stored locally but you need a Google account to use
-            Google Books
-          </p>
+            <p>
+              Finally, you do not need an account to use Dream Reader, your
+              Reading List is stored locally but you need a Google account to
+              use Google Books.
+            </p>
+          </div>
         </div>
       </section>
     </Layout>
