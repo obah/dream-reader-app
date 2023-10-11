c1007ece70714f95c596b3b18d1c7d7b6a1bbc20
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-10-11 11:29:17 +0100

    style: added staggering animation to landing page

--- src/pages/Landing/landing.tsx
+++ src/pages/Landing/landing.tsx
@@ -1,28 +1,64 @@
 import { Layout } from "../../components/layout";
 import { RxArrowTopRight } from "react-icons/rx";
-import "./styles/landing.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import "./styles/landing.css";
 
 export function Landing() {
   return (
     <Layout>
       <section className="landing__section">
         <div className="landing__section-hero">
-          <h3>Hello Reader,</h3>
-          <h1>
+          <motion.h3
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              ease: [0, 0.71, 0.2, 1.01],
+            }}
+          >
+            Hello Reader,
+          </motion.h3>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 1.2,
+              ease: "linear",
+            }}
+          >
             All your favorite books in one place - <br />
             <span>Dream Reader</span>{" "}
-          </h1>
+          </motion.h1>
 
-          <Link to={"/dashboard"}>
-            Launch App <RxArrowTopRight className="button-icon" />
-          </Link>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 1.8,
+              ease: "linear",
+            }}
+          >
+            <Link to={"/dashboard"}>
+              Launch App <RxArrowTopRight className="button-icon" />
+            </Link>
+          </motion.div>
 
-          <p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 1.8,
+              ease: "linear",
+            }}
+          >
             "Books are a uniquely portable magic."
             <br />
             <span>— Stephen King, On Writing: A Memoir of the Craft</span>
-          </p>
+          </motion.p>
         </div>
       </section>
     </Layout>
